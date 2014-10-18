f30d870ed9de2979726d8a7559d2e6efa22b18da
mennaelsadaany <m.elsadaany@hotmail.com>
2014-10-18 18:09:47 -0400

    using threadfork

--- os161-1.99/kern/include/syscall.h
+++ os161-1.99/kern/include/syscall.h
@@ -62,7 +62,7 @@ int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
 int sys_write(int fdesc,userptr_t ubuf,unsigned int nbytes,int *retval);
 void sys__exit(int exitcode);
 int sys_getpid(pid_t *retval);
-int sys_fork(trapframe *tf, pid_t *retval);
+int sys_fork(struct trapframe *tf, pid_t *retval);
 int sys_waitpid(pid_t pid, userptr_t status, int options, pid_t *retval);
 
 #endif // UW
