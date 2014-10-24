099bf47f22629f1a3f9731ee197be8c44a9f6970
mennaelsadaany <m.elsadaany@hotmail.com>
2014-10-24 11:54:40 -0400

    addrspace

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -9,6 +9,7 @@
 #include <thread.h>
 #include <addrspace.h>
 #include <copyinout.h>
+#include <addrspace.h>
 
   /* this implementation of sys__exit does not do anything with the exit code */
   /* this needs to be fixed to get exit() and waitpid() working properly */
