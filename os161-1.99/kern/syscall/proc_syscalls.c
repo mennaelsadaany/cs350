4450a50636730763b7de37847862a212b4d17780
mennaelsadaany <m.elsadaany@hotmail.com>
2014-10-25 03:34:50 -0400

    putting global lock

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -17,7 +17,7 @@
   /* this implementation of sys__exit does not do anything with the exit code */
   /* this needs to be fixed to get exit() and waitpid() working properly */
 
-struct lock *glock; 
+
 
 void sys__exit(int exitcode) {
 
